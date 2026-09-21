33d57fa6328789c1688ff209691edceee5409170
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-21 06:24:47 -0400

    fix: include QFontMetrics explicitly for shader badge

--- src/yuzu/game/game_card.cpp
+++ src/yuzu/game/game_card.cpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include <QFontMetrics>
 #include <QPainter>
 #include <QPainterPath>
 #include "game_card.h"
