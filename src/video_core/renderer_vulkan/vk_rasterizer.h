80137cbba9910d79e171cb85b2d1a67c5ef7c3d7
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-22 14:19:27 -0400

    vulkan: retain depth format for Streamline resources

--- src/video_core/renderer_vulkan/vk_rasterizer.h
+++ src/video_core/renderer_vulkan/vk_rasterizer.h
@@ -151,6 +151,7 @@ public:
         VkExtent2D extent{};
         VkImageSubresourceRange range{};
         VkImageLayout layout{VK_IMAGE_LAYOUT_UNDEFINED};
+        VkFormat format{VK_FORMAT_UNDEFINED};
 
         [[nodiscard]] bool IsValid() const {
             return image != VK_NULL_HANDLE && extent.width != 0 && extent.height != 0;
