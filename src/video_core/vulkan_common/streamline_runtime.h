ea00a41dc89a3e099b1a134dccb526eeee1648d9
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-22 10:15:49 -0400

    streamline: make lifecycle RAII-owned by Vulkan renderer

--- src/video_core/vulkan_common/streamline_runtime.h
+++ src/video_core/vulkan_common/streamline_runtime.h
@@ -1,14 +1,23 @@
 // SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
 // SPDX-License-Identifier: GPL-3.0-or-later
 #pragma once
+
 namespace Vulkan {
-#ifdef HAS_NVIDIA_STREAMLINE
-[[nodiscard]] bool InitializeStreamline();
-void ShutdownStreamline();
-[[nodiscard]] bool IsStreamlineInitialized();
-#else
-inline bool InitializeStreamline() { return false; }
-inline void ShutdownStreamline() {}
-inline bool IsStreamlineInitialized() { return false; }
-#endif
+
+class StreamlineRuntime final {
+public:
+    StreamlineRuntime();
+    ~StreamlineRuntime();
+
+    StreamlineRuntime(const StreamlineRuntime&) = delete;
+    StreamlineRuntime& operator=(const StreamlineRuntime&) = delete;
+
+    [[nodiscard]] bool IsInitialized() const {
+        return initialized;
+    }
+
+private:
+    bool initialized{};
+};
+
 } // namespace Vulkan
