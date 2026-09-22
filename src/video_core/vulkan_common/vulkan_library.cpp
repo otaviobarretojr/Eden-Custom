627df79b9094211e0a376d414f845534e4706cc1
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-22 10:04:12 -0400

    vulkan: gate Streamline loader and verify Windows signature

--- src/video_core/vulkan_common/vulkan_library.cpp
+++ src/video_core/vulkan_common/vulkan_library.cpp
@@ -9,8 +9,10 @@
 #include "common/dynamic_library.h"
 #include "common/fs/path_util.h"
 #include "common/logging.h"
-#ifdef _WIN32
+#if defined(_WIN32) && defined(HAS_NVIDIA_STREAMLINE)
 #include <windows.h>
+#include <softpub.h>
+#include <wintrust.h>
 #endif
 #include "video_core/vulkan_common/vulkan_library.h"
 
@@ -38,13 +40,36 @@ std::shared_ptr<Common::DynamicLibrary> OpenLibrary(
         }
     }
 #else
-#ifdef _WIN32
+#if defined(_WIN32) && defined(HAS_NVIDIA_STREAMLINE)
     // Streamline's Vulkan integration is opt-in. If a signed/packaged interposer is shipped
     // next to Eden, prefer it as the Vulkan loader. If it is absent or incomplete, fall back
     // to the system Vulkan loader without preventing the emulator from starting.
     {
+        wchar_t full_path[MAX_PATH]{};
+        const DWORD path_size = GetFullPathNameW(L"sl.interposer.dll", MAX_PATH, full_path, nullptr);
+        bool signature_valid = false;
+        if (path_size > 0 && path_size < MAX_PATH) {
+            WINTRUST_FILE_INFO file_info{};
+            file_info.cbStruct = sizeof(file_info);
+            file_info.pcwszFilePath = full_path;
+
+            WINTRUST_DATA trust_data{};
+            trust_data.cbStruct = sizeof(trust_data);
+            trust_data.dwUIChoice = WTD_UI_NONE;
+            trust_data.fdwRevocationChecks = WTD_REVOKE_NONE;
+            trust_data.dwUnionChoice = WTD_CHOICE_FILE;
+            trust_data.pFile = &file_info;
+            trust_data.dwStateAction = WTD_STATEACTION_VERIFY;
+            trust_data.dwProvFlags = WTD_CACHE_ONLY_URL_RETRIEVAL;
+
+            GUID policy = WINTRUST_ACTION_GENERIC_VERIFY_V2;
+            signature_valid = WinVerifyTrust(nullptr, &policy, &trust_data) == ERROR_SUCCESS;
+            trust_data.dwStateAction = WTD_STATEACTION_CLOSE;
+            WinVerifyTrust(nullptr, &policy, &trust_data);
+        }
+
         auto streamline = std::make_shared<Common::DynamicLibrary>();
-        if (streamline->Open("sl.interposer.dll")) {
+        if (signature_valid && streamline->Open("sl.interposer.dll")) {
             PFN_vkGetInstanceProcAddr sl_get_instance_proc_addr{};
             PFN_vkGetDeviceProcAddr sl_get_device_proc_addr{};
             const bool has_instance =
@@ -57,6 +82,9 @@ std::shared_ptr<Common::DynamicLibrary> OpenLibrary(
             }
             LOG_WARNING(Render_Vulkan,
                         "Streamline interposer is present but missing Vulkan exports; falling back");
+        } else if (path_size > 0 && path_size < MAX_PATH) {
+            LOG_WARNING(Render_Vulkan,
+                        "Streamline interposer failed Windows signature verification; falling back");
         }
     }
 #endif
