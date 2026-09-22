ed9a6dca018c6b0d42dc424d8d8372a1ca9133a4
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-09-22 08:21:57 -0400

    mod-manager: extend compatible mods dialog for local packages

--- src/yuzu/mod_catalog_dialog.h
+++ src/yuzu/mod_catalog_dialog.h
@@ -7,6 +7,7 @@
 
 #include "common/common_types.h"
 #include "yuzu/mod_catalog.h"
+#include "qt_common/util/mod_library.h"
 
 class QCheckBox;
 class QComboBox;
@@ -24,19 +25,27 @@ public:
 
 private:
     void BuildUi();
+    void ReloadLibrary();
     void ReloadTable();
+    void ImportModPackage();
+    void OpenLibraryFolder();
     void OpenSelectedSource();
     QString CompatibilityText(const EdenCustom::ModCatalogEntry& entry) const;
     bool IsExactMatch(const EdenCustom::ModCatalogEntry& entry) const;
+    bool IsAvailableInLibrary(const EdenCustom::ModCatalogEntry& entry) const;
 
     u64 title_id{};
     QString observed_build_id;
     QVector<EdenCustom::ModCatalogEntry> entries;
+    QVector<QtCommon::ModLibrary::PackageInfo> packages;
 
     QLabel* summary_label{};
+    QLabel* library_label{};
     QLabel* build_id_label{};
     QComboBox* category_filter{};
     QCheckBox* compatible_only{};
     QTableWidget* table{};
     QPushButton* source_button{};
+    QPushButton* import_package_button{};
+    QPushButton* open_library_button{};
 };
